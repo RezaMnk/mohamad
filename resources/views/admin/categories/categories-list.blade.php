43bd862971bfa3be2dbc62ef6d8a4410cb9eb1a9
rezamnk <mirnadaf.reza@gmail.com>
2022-07-06 10:08:56 +0430

    Changes users table

--- resources/views/admin/categories/categories-list.blade.php
+++ resources/views/admin/categories/categories-list.blade.php
@@ -1,14 +1,20 @@
 <ol class="dd-list">
     @foreach($list as $item)
         <li class="dd-item" data-id="{{ $item->id }}">
-            <div class="nested-list-item">
-                <a href="{{ route('admin.categories.edit', $item->id) }}" class="d-inline-block pr-2">
-                    <button type="submit" class="btn btn-default p-0">
-                        <i class="fa fa-pencil font-size-12"></i>
-                    </button>
-                </a>
-
+            <div class="nested-list-item {{ isset($category) ? $category->id == $item->id ? 'bg-light' : '' : '' }}">
                 {{ $item->name }}
+                @unless(isset($category) && $category->id == $item->id)
+                    <a href="{{ route('admin.categories.edit', $item->id) }}" class="btn btn-default p-0 ml-5 font-size-11 text-primary">
+                        ویرایش
+                    </a>
+                    <form action="{{ route('admin.categories.destroy', $item->id) }}" method="post" class="d-inline-block pr-2">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-default p-0 ml-2 font-size-11 text-danger">
+                            حذف
+                        </button>
+                    </form>
+                @endunless
             </div>
             @if(count($item->children))
                 @include('admin.categories.categories-list', ['list' => $item->children])
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,11 +27,47 @@
                                         <i class="fa fa-user"></i>
                                     </div>
                                 </div>
-                                <span class="font-size-13">کاربران</span>
-                                <h2 class="mb-0 ml-auto font-weight-bold text-danger primary-font line-height-30">2,587</h2>
+                                <a href="{{ route('admin.users.unapproved') }}" class="font-size-13">کاربران تایید نشده</a>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-danger primary-font line-height-30">{{ \App\Models\User::count('zarin', false) }} از {{ \App\Models\User::count('all') }}</h2>
                             </div>
                             <div class="progress" style="height: 5px">
-                                <div class="progress-bar bg-danger" role="progressbar" style="width: 71%" aria-valuenow="71" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: {{ \App\Models\User::count('zarin', false) / \App\Models\User::count('all') * 100 }}%" aria-valuenow="{{ \App\Models\User::count('zarin', false) / \App\Models\User::count('all') * 100 }}" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-lg-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-warning icon-block-floating mr-2">
+                                        <i class="fa fa-list-ul"></i>
+                                    </div>
+                                </div>
+                                <a href="{{ route('admin.orders.index') }}" class="font-size-13">سفارشات تایید نشده</a>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-warning primary-font line-height-30">{{ \App\Models\Order::count('unapproved', false) }} از {{ \App\Models\Order::count('all') }}</h2>
+                            </div>
+                            <div class="progress" style="height: 5px">
+                                <div class="progress-bar bg-warning" role="progressbar" style="width: {{ \App\Models\Order::count('unapproved', false) / \App\Models\Order::count('all') * 100 }}%" aria-valuenow="{{ \App\Models\Order::count('unapproved', false) / \App\Models\Order::count('all') * 100 }}" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-lg-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-info icon-block-floating mr-2">
+                                        <i class="fa fa-list-ol"></i>
+                                    </div>
+                                </div>
+                                <a href="{{ route('admin.orders.index') }}" class="font-size-13">سفارشات نهایی نشده</a>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">{{ \App\Models\Order::count('paid', false) }} از {{ \App\Models\Order::count('all') }}</h2>
+                            </div>
+                            <div class="progress" style="height: 5px">
+                                <div class="progress-bar bg-info" role="progressbar" style="width: {{ \App\Models\Order::count('paid', false) / \App\Models\Order::count('all') * 100 }}%" aria-valuenow="{{ \App\Models\Order::count('paid', false) / \App\Models\Order::count('all') * 100 }}" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
                     </div>
@@ -42,50 +78,14 @@
                             <div class="d-flex align-items-center mb-3">
                                 <div>
                                     <div class="icon-block icon-block-sm bg-success icon-block-floating mr-2">
-                                        <i class="fa fa-dollar"></i>
+                                        <i class="fa fa-comment"></i>
                                     </div>
                                 </div>
-                                <span class="font-size-13">مجموع فروش</span>
-                                <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">562</h2>
+                                <a href="{{ route('admin.orders.index') }}" class="font-size-13">پیام های خوانده نشده</a>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">{{ \App\Models\Order::count('approved', false) }} از {{ \App\Models\Order::count('all') }}</h2>
                             </div>
                             <div class="progress" style="height: 5px">
-                                <div class="progress-bar bg-success" role="progressbar" style="width: 39%" aria-valuenow="39" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-6 col-lg-6 col-sm-12">
-                    <div class="card border mb-0">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <div>
-                                    <div class="icon-block icon-block-sm bg-info icon-block-floating mr-2">
-                                        <i class="fa fa-dropbox"></i>
-                                    </div>
-                                </div>
-                                <span class="font-size-13">محصولات</span>
-                                <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">256</h2>
-                            </div>
-                            <div class="progress" style="height: 5px">
-                                <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-6 col-lg-6 col-sm-12">
-                    <div class="card border mb-0">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <div>
-                                    <div class="icon-block icon-block-sm bg-facebook icon-block-floating mr-2">
-                                        <i class="fa fa-list-alt"></i>
-                                    </div>
-                                </div>
-                                <span class="font-size-13">سفارشات</span>
-                                <h2 class="mb-0 ml-auto font-weight-bold text-facebook primary-font line-height-30">147</h2>
-                            </div>
-                            <div class="progress" style="height: 5px">
-                                <div class="progress-bar bg-facebook" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar bg-success" role="progressbar" style="width: {{ \App\Models\Order::count('approved', false) / \App\Models\Order::count('all') * 100 }}%" aria-valuenow="{{ \App\Models\Order::count('approved', false) / \App\Models\Order::count('all') * 100 }}" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
                     </div>
@@ -99,44 +99,38 @@
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
                     جدید ترین سفارشات
-                    <a href="#" class="js-card-refresh link-1">
-                        <i class="fa fa-refresh"></i>
+                    <a href="{{ route('admin.orders.index') }}" class="btn btn-default p-0 font-size-11">
+                        مشاهده همه
                     </a>
                 </div>
                 <div class="card-body pt-2">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">سفارش شماره ۳۴۵۲</h6>
-                                <small class="text-muted">رضا صادری</small>
-                            </div>
-                            <span class="badge badge-danger ml-auto">لغو شده</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">سفارش شماره ۳۴۵۵</h6>
-                                <small class="text-muted">مجتمع کوروش</small>
-                            </div>
-                            <span class="badge badge-success ml-auto">تکمیل شده</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">سفارش شماره ۳۴۵۶</h6>
-                                <small class="text-muted">طلافروشی رضایی</small>
-                            </div>
-                            <span class="badge badge-warning ml-auto">پیش فاکتور</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">سفارش شماره ۳۴۵۹</h6>
-                                <small class="text-muted">طلافروشی صادقی</small>
-                            </div>
-                            <span class="badge badge-primary ml-auto">در انتظار پرداخت</span>
-                        </li>
+                        @foreach(\App\Models\Order::all()->take(4) as $order)
+                            <li class="list-group-item d-flex align-items-center p-l-r-0">
+                                <div>
+                                    <h6 class="m-b-0 primary-font">سفارش شماره {{ $order->id }}</h6>
+                                    <small class="text-muted">{{ $order->user->name }}</small>
+                                </div>
+                                @switch($order->status)
+                                    @case('unapproved')
+                                        <span class="badge badge-warning ml-auto">در انتظار بررسی</span>
+                                        @break
+                                    @case('priced')
+                                        <span class="badge badge-secondary ml-auto">در انتظار پرداخت</span>
+                                        @break
+                                    @case('paid')
+                                        <span class="badge badge-info ml-auto">پرداخت شده</span>
+                                        @break
+                                    @case('approved')
+                                        <span class="badge badge-success ml-auto">تکمیل شده</span>
+                                        @break
+                                    @case('canceled')
+                                        <span class="badge badge-light ml-auto">لغو شده</span>
+                                        @break
+                                @endswitch
+                            </li>
+                        @endforeach
                     </ul>
-                    <a href="#" class="btn btn-block text-center font-size-13 p-b-0">
-                        مشاهده همه
-                    </a>
                 </div>
             </div>
         </div>
@@ -144,44 +138,30 @@
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
                     کاربران جدید
-                    <a href="#" class="js-card-refresh link-1">
-                        <i class="fa fa-refresh"></i>
+                    <a href="{{ route('admin.users.index') }}" class="btn btn-default p-0 font-size-11">
+                        مشاهده همه
                     </a>
                 </div>
                 <div class="card-body pt-2">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">مجتمع کسری</h6>
-                                <small class="text-muted">در زرین یافت شد</small>
-                            </div>
-                            <span class="badge badge-danger ml-auto">غیرفعال</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">طلای صحرا</h6>
-                                <small class="text-muted">در زرین یافت نشد</small>
-                            </div>
-                            <span class="badge badge-success ml-auto">تایید شده</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">طلای ناصری</h6>
-                                <small class="text-muted">در زرین یافت شد</small>
-                            </div>
-                            <span class="badge badge-warning ml-auto">VIP</span>
-                        </li>
-                        <li class="list-group-item d-flex align-items-center p-l-r-0">
-                            <div>
-                                <h6 class="m-b-0 primary-font">طلای صدر</h6>
-                                <small class="text-muted">در زرین یافت شد</small>
-                            </div>
-                            <span class="badge badge-primary ml-auto">در انتظار تایید</span>
-                        </li>
+                        @foreach(\App\Models\User::whereAdmin(false)->latest()->take(4)->get() as $user)
+                            <li class="list-group-item d-flex align-items-center p-l-r-0">
+                                <div>
+                                    <h6 class="m-b-0 primary-font">{{ $user->name }}</h6>
+                                    <small class="text-muted">
+                                        @if($user->zarin)
+                                            کاربر زرین
+                                        @else
+                                            در زرین یافت شد
+                                        @endif
+                                    </small>
+                                </div>
+                                @if($user->vip)
+                                    <span class="badge badge-warning ml-auto">VIP</span>
+                                @endif
+                            </li>
+                        @endforeach
                     </ul>
-                    <a href="#" class="btn btn-block text-center font-size-13 p-b-0">
-                        مشاهده همه
-                    </a>
                 </div>
             </div>
         </div>
