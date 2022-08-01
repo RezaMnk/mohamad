3351df0bb9386a575e3febccfbb310bd1402133e
RezaMnk <mirnadaf.reza@gmail.com>
2022-08-01 12:04:25 +0430

    Updated .env

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit35083529a74db8ba9400b9ee65a2afd3
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit35083529a74db8ba9400b9ee65a2afd3', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit35083529a74db8ba9400b9ee65a2afd3', 'loadClassLoader'));
