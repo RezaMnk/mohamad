39013c11889b482a665906787d3d578456faa031
Mohammad Tajrishi <tajrishi.moh@gmail.com>
2022-08-04 17:18:07 +0430

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,18 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-dark border border-light">
+                
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
+                        
                         @csrf
-
-                        @if($errors->any())
-                            @foreach($errors->all() as $error)
-                                <p class="text-danger">{{ $error }}</p>
-                            @endforeach
-                        @endif
-
                         <div class="row mb-3">
                             <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Phone Number') }}</label>
 
@@ -53,7 +48,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-4 d-none">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -65,9 +60,12 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
+                                </button>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('signup') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
