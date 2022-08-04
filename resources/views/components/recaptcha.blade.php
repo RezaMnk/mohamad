2a0e2b9e91fc063cfc5fa6c42131f5578e04744b
RezaMnk <mirnadaf.reza@gmail.com>
2022-08-04 17:06:23 +0430

    Recaptcha dark

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -1,5 +1,5 @@
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<div class="g-recaptcha" @class(['is-invalid' => $hasError]) data-sitekey="{{ env('GOOGLE_RECAPTCHA_SITE_KEY') }}"></div>
+<div class="g-recaptcha" @class(['is-invalid' => $hasError]) data-theme="dark" data-sitekey="{{ env('GOOGLE_RECAPTCHA_SITE_KEY') }}"></div>
 @error('g-recaptcha-response')
     <span class="invalid-feedback" role="alert">
         <strong>{{ $message }}</strong>
